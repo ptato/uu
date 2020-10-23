11c6f050d10654ab94a0112584de17989a0774ed
Pablo Tato Ramos <pablo.tato@vipera.com>
2020-10-23 14:39:47 +0200

    A

--- src/editor.zig
+++ src/editor.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 const ArrayList = std.ArrayList;
 
 pub const Buffer = struct {
+    allocator: *std.mem.Allocator,
     lines: ArrayList(ArrayList(u8)),
 
     pub fn create(allocator: *std.mem.Allocator) Buffer {
         return Buffer {
+            .allocator=allocator,
             .lines=ArrayList(ArrayList(u8)).init(allocator),
         };
     }
@@ -15,5 +17,16 @@ pub const Buffer = struct {
             line.deinit();
         }
         self.lines.deinit();
+    }
+
+    pub fn append(self: *Buffer, chars: []u8) void {
+        if (self.lines.items.len == 0) {
+            self.lines.append(ArrayList(u8).init(self.allocator)) catch unreachable;
+        }
+        self.lines.items[0].appendSlice(chars) catch unreachable;
+    }
+
+    pub fn toString(self: *Buffer) []u8 {
+        return self.lines.items[0].items;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -5,34 +5,57 @@ usingnamespace @cImport({
 const std = @import("std");
 const editor = @import("editor.zig");
 
-const allocator = std.heap.c_allocator;
+const default_allocator = std.heap.c_allocator;
 
+const App = struct {
+    webview: webview_t,
+    buffer: editor.Buffer,
+
+    pub fn create(allocator: *std.mem.Allocator, webview: webview_t) App {
+        return App {
+            .webview = webview,
+            .buffer = editor.Buffer.create(allocator),
+        };
+    }
+
+    pub fn deinit(self: App) void {
+        self.buffer.deinit();
+    }
+};
 
 export fn onKeyDown(seq: [*c]const u8, req: [*c]const u8, arg: ?*c_void) void {
+    var app = @ptrCast(*App, @alignCast(8, arg));
+
     var parser = std.json.TokenStream.init(std.mem.span(req));
-    const parsedReq = std.json.parse([][]u8, &parser, std.json.ParseOptions{ .allocator=allocator }) catch unreachable;
+    const parsedReq = std.json.parse([][]u8, &parser, std.json.ParseOptions{ .allocator=app.allocator }) catch unreachable;
     const key = parsedReq[0];
-    webview_return(arg, seq, 0, req);
+    app.buffer.append(key);
+
+    const text = app.buffer.toString();
+    const js = std.fmt.allocPrint(app.allocator, "state.text = '{}'\x00", .{text}) catch return;
+    defer app.allocator.free(js);
+    webview_eval(app.webview, @ptrCast([*c]const u8, js));
 }
 
 // https://ziglang.org/documentation/0.6.0/#Choosing-an-Allocator
 // https://ziglearn.org/chapter-2/
 pub fn main() anyerror!void {
 
-    var buffer = editor.Buffer.create(allocator);
-    defer buffer.deinit();
-
     const html = @embedFile("index.html");
-    const data_uri = try std.fmt.allocPrint(allocator, " data:text/html,{}\x00", .{html});
-    defer allocator.free(data_uri);
+    const data_uri = try std.fmt.allocPrint(default_allocator, " data:text/html,{}\x00", .{html});
+    defer default_allocator.free(data_uri);
     const data_uri_c = @ptrCast([*c]const u8, data_uri);
 
     const debug = 1;
     var w = webview_create(debug, null);
+
+    var app = App.create(default_allocator, w);
+    defer app.deinit();
+
     webview_set_title(w, "uu");
     webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
     webview_navigate(w, data_uri_c);
-    webview_bind(w, "uu_onKeyDown", onKeyDown, w);
+    webview_bind(w, "uu_onKeyDown", onKeyDown, &app);
     webview_run(w);
     webview_destroy(w);
 }
